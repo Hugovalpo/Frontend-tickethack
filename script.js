3ed675a18e43e6c5ad19e6fd23da9e6e7a27199e
Hugo Lillo <lillo.hugo@gmail.com>
2022-09-21 09:28:07 +0200

    Tickethand-frontend

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log();
+
+document.querySelector('#Search').addEventListener('click', function () {
+	const trip = {
+        
+        Departure: document.querySelector('#registerDeparture').value,
+        Arrival: document.querySelector('#registerArrival').value,
+        Date: document.querySelector('#registerDate').value,
+    };
+
+	fetch('http://localhost:3000/trips', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ Departure }),
+	}).then(response => response.json())
+		.then(data => {
+			if (data.result) {
+                //si le voyage exist, ils s'affichent tous dans la card droite
+                document.querySelector('#triplist').innerHTML += 
+
+
+
+            }
